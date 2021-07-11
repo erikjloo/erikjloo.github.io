2055739dfecceb3583185ec1f7bcdedfd61ab0f1
Erik <erik.giesenloo@gmail.com>
2021-07-11 13:37:47 +0100

    footer to bottom

--- js/index.js
+++ js/index.js
@@ -20,9 +20,8 @@ navLinks.forEach(link => {
 //===========================================================================
 
 const appearOptions = {
-    threshold: 1,
+    threshold: 0,
     rootMargin: "0px 0px -200px 0px"
-
 };
 
 const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
