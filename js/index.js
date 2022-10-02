569fef651eac23a454c548a7dd821958e2057d75
Erik <erik.giesenloo@gmail.com>
2022-10-02 21:52:48 +0100

    reversed header animation

--- js/index.js
+++ js/index.js
@@ -4,17 +4,17 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 const faders = document.querySelectorAll('.fade-in');
 
+
+// document.body.classList.add('nav-open')
+
 navToggle.addEventListener('click', () => {
-    // Print F12 - go to console
-    // console.log("I am working");
-    // console.log(document.querySelector('.nav-open'));
-    document.body.classList.toggle('nav-open');
+  document.body.classList.toggle('nav-close');
 });
 
 navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
+  link.addEventListener('click', () => {
+    document.body.classList.add('nav-close');
+  })
 });
 
 //===========================================================================
@@ -22,23 +22,23 @@ navLinks.forEach(link => {
 //===========================================================================
 
 const appearOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px -50px 0px"
+  threshold: 0,
+  rootMargin: "0px 0px -50px 0px"
 };
 
 const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) { 
-            return; 
-        } else {
-            entry.target.classList.add('appear');   // Fade-in
-            appearOnScroll.unobserve(entry.target); // Stop checking for fade-in
-        }
-    })
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) { 
+      return; 
+    } else {
+      entry.target.classList.add('appear');   // Fade-in
+      appearOnScroll.unobserve(entry.target); // Stop checking for fade-in
+    }
+  })
 }, appearOptions);
 
 faders.forEach(fader => {
-    appearOnScroll.observe(fader);
+  appearOnScroll.observe(fader);
 })
 
 //===========================================================================
@@ -47,53 +47,32 @@ faders.forEach(fader => {
 
 var imageContainer = document.querySelector(".intro__image__container")
 var imageArray = ["./images/backgrounds/panorama2.jpg", 
-                  "./images/backgrounds/panorama3.jpg",
-                  "./images/backgrounds/panorama4.jpg",
-                  "./images/backgrounds/panorama5.jpg",
-                  "./images/backgrounds/panorama6.jpg"]
+          "./images/backgrounds/panorama3.jpg",
+          "./images/backgrounds/panorama4.jpg",
+          "./images/backgrounds/panorama5.jpg",
+          "./images/backgrounds/panorama6.jpg"]
 var imageIndex = 0;
 
-function whichTransitionEvent(){
-    var t;
-    var el = document.createElement('fakeelement');
-    var transitions = {
-      'transition':'transitionend',
-      'OTransition':'oTransitionEnd',
-      'MozTransition':'transitionend',
-      'WebkitTransition':'webkitTransitionEnd'
-    }
-
-    for(t in transitions){
-        if( el.style[t] !== undefined ){
-            return transitions[t];
-        }
-    }
-}
-var transitionEnd = whichTransitionEvent();
 function swapImage() {
 
-    image = document.querySelector(".intro__image");
-    
-    // We add a new image behind our current image
-    var newImage = new Image();
-    newImage.src = imageArray[imageIndex];
-    newImage.className = "intro__image";
-    imageContainer.insertBefore(newImage, image.nextSibling);
+  image = document.querySelector(".intro__image");
+  
+  // We add a new image behind our current image
+  let newImage = new Image();
+  newImage.src = imageArray[imageIndex];
+  newImage.className = "intro__image";
+  imageContainer.insertBefore(newImage, image.nextSibling);
 
-    // Transition end is only detected if a class is added
-    image.classList.add("intro__image__fade-out");
-    // Synchronous transitions can only happen if animate is used
-    newImage.animate( { opacity: 1 }, 2000);
-    // Add event listener works much better than setTimeout
-    image.addEventListener(transitionEnd, removeImage);
+  // Transition end is only detected if a class is added
+  image.classList.add("intro__image__fade-out");
+  newImage.classList.add("intro__image__fade-in");
+  image.addEventListener('transitionend', () => { image.remove(); }, {once: true});
 
-    imageIndex++;
-    if (imageIndex >= imageArray.length) {
-        imageIndex = 0;
-    }
+  imageIndex++;
+  imageIndex %= imageArray.length;
+  // if (imageIndex >= imageArray.length) {
+  //   imageIndex = 0;
+  // }
 }
-function removeImage(event) { 
-    image.removeEventListener(transitionEnd, removeImage);
-    imageContainer.removeChild(image); 
-}
-setInterval(swapImage, 20000);
+
+setInterval(swapImage, 10);
