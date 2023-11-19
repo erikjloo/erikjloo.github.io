ea49e293271c61a2edef1ccb0cebe1d11cab9e75
Erik <erik.giesenloo@gmail.com>
2023-11-19 17:48:59 +0000

    Improved index intro picture slides

--- js/index.js
+++ js/index.js
@@ -51,15 +51,16 @@ faders.forEach(fader => {
 // Switch background images
 //===========================================================================
 
-var imageArray = ["./images/backgrounds/panorama2.jpg", 
+var imageArray = ["./images/backgrounds/panorama0.jpg", 
+                  "./images/backgrounds/panorama1.jpg",
+                  "./images/backgrounds/panorama2.jpg",
                   "./images/backgrounds/panorama3.jpg",
                   "./images/backgrounds/panorama4.jpg",
                   "./images/backgrounds/panorama5.jpg",
-                  "./images/backgrounds/panorama6.jpg"]
+                  ]
 var imageIndex = 0;
                   
 function swapIntroImage() {
-
   imageIndex++;
   imageIndex %= imageArray.length;
   swapImage("intro__image", imageArray[imageIndex])
