f9ee0d0cf3f635e30976bf2fc0019d49eb2c53d0
Erik <erik.giesenloo@gmail.com>
2023-11-21 20:57:23 +0000

    gradients (wip)

---
+++ js/gradients.js
@@ -0,0 +1,6 @@
+// $("#color1").change(function () {
+//     $("h1").css('background', $(this).val());
+// });
+var colorPickers = []
+let val = document.getElementById("color1").value; 
+console.log(val)
